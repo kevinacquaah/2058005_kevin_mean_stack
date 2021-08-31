181d052b443c8f13cd08053ebddf9bcfb0e32fe7
Kevin Acquaah <55247237+kevinacquaah@users.noreply.github.com>
2021-08-31 15:37:13 -0400

    Add files via upload

---
+++ Phase 3/ChattingWithSocketIO/server.js
@@ -0,0 +1,23 @@
+
+const express = require("express")
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
+const PORT = 4200
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html")
+
+
+})
+
+io.on("connection",(socket)=>{
+    console.log("client connected to application..........")
+
+    socket.on("chat", (msg)=>{
+        // We need to unpack the message & display the message
+        console.log(`[${msg['name']}]:  ${msg['msg']}`)
+    })
+})
+http.listen(PORT,()=>console.log(`[LOG]: Listening @ http://localhost:${PORT}/`))
